39a891aac7eaa0b4f698b6323aff6303e95443c9
Hazmen <speedcubing234@gmail.com>
2025-11-27 14:29:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,295 +1,336 @@
-let inp = document.querySelector('.input')
-let counter = document.querySelector('.steps-count')
-let steps = document.querySelector('.steps')
-let btn = document.querySelector('.btn')
-let maxstat = document.querySelector('.max-stat')
-let copyBtn = document.querySelector('.copy')
-let currentNum = document.querySelector('.currentNumDig')
+document.addEventListener('DOMContentLoaded', () => {
+    let inp = document.querySelector('.input')
+    let counter = document.querySelector('.steps-count')
+    let steps = document.querySelector('.steps')
+    let btn = document.querySelector('.btn')
+    let maxstat = document.querySelector('.max-stat')
+    let copyBtn = document.querySelector('.copy')
+    let currentNum = document.querySelector('.currentNumDig')
 
-let speedRange = document.getElementById('speed')
-let valueLabel = document.getElementById('speedValue')
-let currentSpeed = speedRange.value;
-let stepSpeed = 750;
+    let speedRange = document.getElementById('speed')
+    let valueLabel = document.getElementById('speedValue')
+    let currentSpeed = speedRange.value;
+    let stepSpeed = 750;
 
-let isRunning = false;
-let currentTimeout = null;
+    let isRunning = false;
+    let currentTimeout = null;
 
-window.CollatzApp = {
-    lastList: [],
-    onNewList: null,
-};
+    window.CollatzApp = {
+        lastList: [],
+        onNewList: null,
+    };
 
-inp.addEventListener("beforeinput", e => {
-    if (e.data && !/^\d+$/.test(e.data)) {
-        e.preventDefault();
-    }
-});
+    inp.addEventListener("beforeinput", e => {
+        if (e.data && !/^\d+$/.test(e.data)) {
+            e.preventDefault();
+        }
+    });
 
-function maxOfList(list) {
-    let max = null;
+    function maxOfList(list) {
+        let max = null;
 
-    for (let v of list) {
-        if (typeof v === "number" || typeof v === "bigint") {
-            if (max === null || v > max) {
-                max = v;
+        for (let v of list) {
+            if (typeof v === "number" || typeof v === "bigint") {
+                if (max === null || v > max) {
+                    max = v;
+                }
             }
         }
+
+        return max;
     }
 
-    return max;
-}
+    function shortenNumForCND(value, mode = 'auto') {
+        let str = (typeof value === 'bigint') ? value.toString() : String(value);
 
-function speedRangeGetValue(){
-    speedRange.addEventListener('input', () => {
-        currentSpeed = Number(speedRange.value)
-        valueLabel.innerHTML = currentSpeed + 'x';
-        if (currentSpeed == 1) {
-            stepSpeed = 750;
-        } else if (currentSpeed == 2) {
-            stepSpeed = 150;
-        } else if (currentSpeed == 3) {
-            stepSpeed = 50;
-        } else if (currentSpeed == 4) {
-            stepSpeed = 10;
-        } else if (currentSpeed == 5) {
-            stepSpeed = false;
+        if (str.length <= 12) return str;
+
+        if (mode === 'dots') {
+            return str.slice(0, 6) + '...' + str.slice(-4);
         }
+
+        if (mode === 'e') {
+            const first = str[0];
+            const after = str.slice(1);
+            const exp = after.length;
+            return `${first}.${after.slice(0, 3)}e+${exp}`;
+        }
+
+        if (mode === 'auto') {
+            if (str.length <= 15) {
+                const first = str[0];
+                const after = str.slice(1);
+                const exp = after.length;
+                return `${first}.${after.slice(0, 3)}e+${exp}`;
+            }
+        }
+
+        return str;
+    }
+
+    function speedRangeGetValue(){
+        speedRange.addEventListener('input', () => {
+            currentSpeed = Number(speedRange.value)
+            valueLabel.innerHTML = currentSpeed + 'x';
+            if (currentSpeed == 1) {
+                stepSpeed = 750;
+            } else if (currentSpeed == 2) {
+                stepSpeed = 150;
+            } else if (currentSpeed == 3) {
+                stepSpeed = 50;
+            } else if (currentSpeed == 4) {
+                stepSpeed = 10;
+            } else if (currentSpeed == 5) {
+                stepSpeed = false;
+            }
+        })
+    }
+
+    // Способ с рекурсивным TimeOut
+    function playSteps(list, i = 0) {
+        if (i >= list.length) {
+            isRunning = false;
+            return;
+        }
+
+        const value = list[i];
+        const p = document.createElement('p');
+        
+        p.textContent = (typeof value === 'number' || typeof value === 'bigint')
+            ? `${i + 1}. ${value}` 
+            : value;
+
+        steps.appendChild(p);
+        steps.scrollTo({ top: steps.scrollHeight });
+
+        // Работаем только с уже показанными шагами
+        const shown = list.slice(0, i + 1);
+
+        // Количество шагов (только числа)
+        const stepsCount = shown.filter(x => typeof x === 'number' || typeof x === 'bigint').length;
+        counter.textContent = `количество шагов: ${stepsCount}`;
+
+        // Максимум среди показанных чисел
+        const max = maxOfList(shown);
+        maxstat.textContent = `самое большое число: ${max === null ? '-' : String(max)}`;
+
+        // Текущее число для индикатора
+        // currentNum.textContent = (typeof value === 'number' || typeof value === 'bigint') ? String(value) : '1';
+
+        currentNum.textContent = (typeof value === 'number' || typeof value === 'bigint')
+                                 ? shortenNumForCND(value, 'e') : '1';
+
+        currentTimeout = setTimeout(() => {
+            playSteps(list, i + 1);
+        }, stepSpeed);
+    }
+
+
+
+    speedRangeGetValue();
+
+    function IntOrBigInt(inputValue) {
+        const s = inputValue.trim();
+
+        if (!/^[0-9]+$/.test(s)) {
+            return { error: "Введите положительное число."};
+        }
+
+        const normalized = s.split('.')[0]
+
+        if (normalized.length > 2) {
+            return { value: BigInt(s), isBig: true };
+        }
+
+        return { value: Number(s), isBig: false };
+    }
+
+    function changeForASec(item, changetxt, time){
+        old = item.textContent
+        item.textContent = changetxt
+        setTimeout(() => {item.innerHTML = old;}, time)
+    }
+
+    copyBtn.addEventListener('click', () => {
+        const parsed = IntOrBigInt(inp.value);
+
+        const spisok = collatzSmart(parsed.value, parsed.isBig);
+
+        // const text = steps.innerText;
+        const numsOnly = spisok.filter(x => typeof x === "number" || typeof x === "bigint");
+        // new Text(numsOnly)
+        const textToCopy = numsOnly.join('\n');
+
+
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                changeForASec(copyBtn, 'Скопировано!', 500)
+            })
+            .catch(err => {
+                changeForASec(copyBtn, `Error: ${err}`, 500)
+            })
     })
-}
 
-// Способ с рекурсивным TimeOut
-function playSteps(list, i = 0) {
-    if (i >= list.length) {
-        isRunning = false;
-        return;
+    function isEven(number){
+        return number % 2 === 0;
     }
 
-    const value = list[i];
-    const p = document.createElement('p');
-    p.textContent = (typeof value === 'number' || typeof value === 'bigint')
-        ? `${i + 1}. ${value}` 
-        : value;
-    steps.appendChild(p);
-    steps.scrollTo({ top: steps.scrollHeight });
+    btn.addEventListener('click', function(){
+        steps.textContent = '';
 
-    // Работаем только с уже показанными шагами
-    const shown = list.slice(0, i + 1);
+        const parsed = IntOrBigInt(inp.value);
+        console.log('parsed:', parsed, typeof parsed.value);
+        // const parsed = IntOrBigInt(inp.value);
+        if (parsed.error) {
+            steps.textContent = parsed.error;
+            return;
+        }
 
-    // Количество шагов (только числа)
-    const stepsCount = shown.filter(x => typeof x === 'number' || typeof x === 'bigint').length;
-    counter.textContent = `количество шагов: ${stepsCount}`;
-
-    // Максимум среди показанных чисел
-    const max = maxOfList(shown);
-    maxstat.textContent = `самое большое число: ${max === null ? '-' : String(max)}`;
-
-    // Текущее число для индикатора
-    currentNum.textContent = (typeof value === 'number' || typeof value === 'bigint') ? String(value) : '1';
-
-    currentTimeout = setTimeout(() => {
-        playSteps(list, i + 1);
-    }, stepSpeed);
-}
+        const spisok = collatzSmart(parsed.value, parsed.isBig);
 
 
+        window.CollatzApp.lastList = spisok;
 
-speedRangeGetValue();
+        if (typeof window.CollatzApp.onNewList === 'function') {
+            window.CollatzApp.onNewList(spisok);
+        }
 
-function IntOrBigInt(inputValue) {
-    const s = inputValue.trim();
-
-    if (!/^[0-9]+$/.test(s)) {
-        return { error: "Введите положительное число."};
-    }
-
-    const normalized = s.split('.')[0]
-
-    if (normalized.length > 32) {
-        return { value: BigInt(s), isBig: true };
-    }
-
-    return { value: Number(s), isBig: false };
-}
-
-function changeForASec(item, changetxt, time){
-    old = item.textContent
-    item.textContent = changetxt
-    setTimeout(() => {item.innerHTML = old;}, time)
-}
-
-copyBtn.addEventListener('click', () => {
-    const parsed = IntOrBigInt(inp.value);
-
-    const spisok = collatzSmart(parsed.value, parsed.isBig);
-
-    // const text = steps.innerText;
-    const numsOnly = spisok.filter(x => typeof x === "number");
-    // new Text(numsOnly)
-    const textToCopy = numsOnly.join('\n');
+        if(isRunning) {
+            clearTimeout(currentTimeout);
+            isRunning = false;
+        }
 
 
-    navigator.clipboard.writeText(textToCopy)
-        .then(() => {
-            changeForASec(copyBtn, 'Скопировано!', 500)
-        })
-        .catch(err => {
-            changeForASec(copyBtn, `Error: ${err}`, 500)
-        })
-})
+        isRunning = true;
+        playSteps(spisok);
 
-function isEven(number){
-    return number % 2 === 0;
-}
+        
+        // Способ выполнения вычисления за раз
+        // steps.innerHTML = spisok
+        // // .slice(0, spisok.length - 1)
+        // .map((x, i) => { 
+        //     if (i === spisok.length - 1){
+        //         return `<p>${x}</p>`
+        // } else { 
+        //     return `<p>${i + 1}. ${x}</p>`
+        // }})
+        // .join('');
 
-btn.addEventListener('click', function(){
-    steps.textContent = '';
+    // ===========================================
 
-    const parsed = IntOrBigInt(inp.value);
-    if (parsed.error) {
-        steps.textContent = parsed.error;
-        return;
-    }
+        // Способ вычисления пошагово
+        // let i = 0;
+        // const interval = setInterval(() => {
+        //     if (i >= spisok.length) {
+        //         clearInterval(interval);
+        //         return;
+        //     }
 
-    const spisok = collatzSmart(parsed.value, parsed.isBig);
+        //     const value = spisok[i];
+        //     const p = document.createElement('p');
+        //     p.textContent = (typeof value === 'number')? `${i + 1}. ${value}`: value;
+        //     steps.appendChild(p);
+            
+        //     i++
+        // }, stepSpeed)
+        
+    // ==========================================
 
-    window.CollatzApp.lastList = spisok;
+        // const list = collatz2(Number(inp.value));
 
-    if (typeof window.CollatzApp.onNewList === 'function') {
-        window.CollatzApp.onNewList(spisok);
-    }
-
-    if(isRunning) {
-        clearTimeout(currentTimeout);
-        isRunning = false;
-    }
+        
+    })
 
 
-    isRunning = true;
-    playSteps(spisok);
+    // btn.addEventListener('click', () => {
+    //     if (isRunning) return;
 
-    
-    // Способ выполнения вычисления за раз
-    // steps.innerHTML = spisok
-    // // .slice(0, spisok.length - 1)
-    // .map((x, i) => { 
-    //     if (i === spisok.length - 1){
-    //         return `<p>${x}</p>`
-    // } else { 
-    //     return `<p>${i + 1}. ${x}</p>`
-    // }})
-    // .join('');
+    //     isRunning = true;
+    //     steps.textContent = "";
+    //     const list = collatz2(Number(inp.value));
 
-// ===========================================
+    //     playSteps(list);
+    // });
 
-    // Способ вычисления пошагово
-    // let i = 0;
-    // const interval = setInterval(() => {
-    //     if (i >= spisok.length) {
-    //         clearInterval(interval);
-    //         return;
+
+    // function collatz(n){
+    //     while (n !== 1){
+    //         if (n % 2 === 0) {
+    //             n = n / 2
+    //             steps.textContent = n
+    //         } else {
+    //             n = n * 3 + 1
+    //             steps.textContent = n
+    //         }
+    //     }
+    // }
+
+    // function collatz2(n){
+    //     const spisok = [n];
+    //     if (n <= 0) { 
+    //         steps.textContent = "Введи положительное число" 
+    //         counter.textContent = '';
+    //         maxstat = '';
+    //         return [];
     //     }
 
-    //     const value = spisok[i];
-    //     const p = document.createElement('p');
-    //     p.textContent = (typeof value === 'number')? `${i + 1}. ${value}`: value;
-    //     steps.appendChild(p);
         
-    //     i++
-    // }, stepSpeed)
-    
-// ==========================================
+    //     while (n !== 1) { 
+    //         n = (n % 2 === 0)? n / 2 : 3*n+1;
+    //         spisok.push(n) // добавляет n в списокк
+    //         if (spisok.length > 10000) {
+    //             spisok.push("Много шагов, у сайта приступ")
+    //         }
 
-    // const list = collatz2(Number(inp.value));
+    //         if (n == 1) {
+    //             spisok.push("Ты достиг бесконечного цикла!")
+    //         }
+    //     }
 
+    //     // counter.innerHTML = `количество шагов: \n ${spisok.length - 1}`
+
+    //     // const numsOnly = spisok.filter(x => typeof x === "number");
+    //     // const max = Math.max(...numsOnly) //... < распаковывает список в элементы
+    //     // maxstat.innerHTML = `самое большое число: \n ${max}`
     
+    //     return spisok;
+    // }
+
+    function collatzSmart(n, isBig) {
+        console.log('collatzSmart start, n:', n, typeof n, 'isBig:', isBig);
+        const spisok = [n];
+
+        const ONE = isBig ? 1n : 1;
+        const TWO = isBig? 2n : 2;
+        const THREE = isBig? 3n : 3;
+
+        while (n !== ONE) {
+            if (n % TWO === (isBig ? 0n : 0)) {
+                n = n / TWO;
+            } else {
+                n = n * THREE + ONE
+            }
+
+            spisok.push(n);
+
+            if (spisok.length > 16000) {
+                spisok.push('Отличное число, но шаги вызывают приступ у сайта!');
+                break;
+            }
+        }
+        
+        if (n === ONE) {
+            spisok.push("Цикл достигнут.")
+        }
+
+        return spisok;
+    }
 })
 
 
-// btn.addEventListener('click', () => {
-//     if (isRunning) return;
 
-//     isRunning = true;
-//     steps.textContent = "";
-//     const list = collatz2(Number(inp.value));
-
-//     playSteps(list);
-// });
-
-
-function collatz(n){
-    while (n !== 1){
-        if (n % 2 === 0) {
-            n = n / 2
-            steps.textContent = n
-        } else {
-            n = n * 3 + 1
-            steps.textContent = n
-        }
-    }
-}
-
-function collatz2(n){
-    const spisok = [n];
-    if (n <= 0) { 
-        steps.textContent = "Введи положительное число" 
-        counter.textContent = '';
-        maxstat = '';
-        return [];
-    }
-
-    
-    while (n !== 1) { 
-        n = (n % 2 === 0)? n / 2 : 3*n+1;
-        spisok.push(n) // добавляет n в списокк
-        if (spisok.length > 10000) {
-            spisok.push("Много шагов, у сайта приступ")
-        }
-
-        if (n == 1) {
-            spisok.push("Ты достиг бесконечного цикла!")
-        }
-    }
-
-    // counter.innerHTML = `количество шагов: \n ${spisok.length - 1}`
-
-    // const numsOnly = spisok.filter(x => typeof x === "number");
-    // const max = Math.max(...numsOnly) //... < распаковывает список в элементы
-    // maxstat.innerHTML = `самое большое число: \n ${max}`
- 
-    return spisok;
-}
-
-function collatzSmart(n, isBig) {
-    const spisok = [n];
-
-    const ONE = isBig ? 1n : 1;
-    const TWO = isBig? 2n : 2;
-    const THREE = isBig? 3n : 3;
-
-    while (n !== ONE) {
-        if (n % TWO === 0) {
-            n = n / TWO;
-        } else {
-            n = n * THREE + ONE
-        }
-
-        spisok.push(n);
-
-        if (spisok.length > 16000) {
-            spisok.push('Отличное число, но шаги вызывают приступ у сайта!');
-            break;
-        }
-    }
-    
-    if (n === ONE) {
-        spisok.push("Цикл достигнут.")
-    }
-
-    return spisok;
-}
-
-
-
+console.log("speed =", document.getElementById("speed"));
+console.log("sim-graph =", document.getElementById("sim-graph"));
 
 
 
